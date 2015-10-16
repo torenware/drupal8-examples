b315ef17a19af865da29441e0901a6c087ac9893
Rob Thorne <rob@torenware.com>
2015-10-15 23:04:48 -0700

    The stream implementation loads.  Who knows if it works?

--- file_example/src/StreamWrapper/FileExampleSessionStreamWrapper.php
+++ file_example/src/StreamWrapper/FileExampleSessionStreamWrapper.php
@@ -10,6 +10,7 @@
 namespace Drupal\file_example\StreamWrapper;
 
 use Drupal\Core\StreamWrapper\StreamWrapperInterface;
+use Drupal\Core\StreamWrapper;
 use Drupal\Core\Url;
 
 /**
@@ -43,12 +44,13 @@ use Drupal\Core\Url;
  * @ingroup file_example
  */
 class FileExampleSessionStreamWrapper implements StreamWrapperInterface {
+
   /**
-   * Stream context resource.
+   * A generic resource handle.
    *
-   * @var Resource
+   * @var resource
    */
-  public $context;
+  public $handle = NULL;
 
 
   /**
@@ -119,22 +121,6 @@ class FileExampleSessionStreamWrapper implements StreamWrapperInterface {
   }
 
 
-  /**
-   * Returns canonical, absolute path of the resource.
-   *
-   * Implementation placeholder. PHP's realpath() does not support stream
-   * wrappers. We provide this as a default so that individual wrappers may
-   * implement their own solutions.
-   *
-   * @return string
-   *   Returns a string with absolute pathname on success (implemented
-   *   by core wrappers), or FALSE on failure or if the registered
-   *   wrapper does not provide an implementation.
-   */
-  public function realpath() {
-    return FALSE; //??
-  }
-  
   /**
    * Implements setUri().
    */
@@ -233,7 +219,16 @@ class FileExampleSessionStreamWrapper implements StreamWrapperInterface {
   }
 
   /**
-   * Implements realpath().
+   * Returns canonical, absolute path of the resource.
+   *
+   * Implementation placeholder. PHP's realpath() does not support stream
+   * wrappers. We provide this as a default so that individual wrappers may
+   * implement their own solutions.
+   *
+   * @return string
+   *   Returns a string with absolute pathname on success (implemented
+   *   by core wrappers), or FALSE on failure or if the registered
+   *   wrapper does not provide an implementation.
    */
   public function realpath() {
     return 'session://' . $this->getLocalPath();
@@ -330,6 +325,115 @@ class FileExampleSessionStreamWrapper implements StreamWrapperInterface {
     }
     return $var;
   }
+  
+  /**
+   * Retrieve the underlying stream resource.
+   *
+   * This method is called in response to stream_select().
+   *
+   * @param int $cast_as
+   *   Can be STREAM_CAST_FOR_SELECT when stream_select() is calling
+   *   stream_cast() or STREAM_CAST_AS_STREAM when stream_cast() is called for
+   *   other uses.
+   *
+   * @return resource|false
+   *   The underlying stream resource or FALSE if stream_select() is not
+   *   supported.
+   *
+   * @see stream_select()
+   * @see http://php.net/manual/streamwrapper.stream-cast.php
+   */
+  public function stream_cast($cast_as) {
+    return FALSE;
+  }
+  
+  /**
+   * Sets metadata on the stream.
+   *
+   * @param string $path
+   *   A string containing the URI to the file to set metadata on.
+   * @param int $option
+   *   One of:
+   *   - STREAM_META_TOUCH: The method was called in response to touch().
+   *   - STREAM_META_OWNER_NAME: The method was called in response to chown()
+   *     with string parameter.
+   *   - STREAM_META_OWNER: The method was called in response to chown().
+   *   - STREAM_META_GROUP_NAME: The method was called in response to chgrp().
+   *   - STREAM_META_GROUP: The method was called in response to chgrp().
+   *   - STREAM_META_ACCESS: The method was called in response to chmod().
+   * @param mixed $value
+   *   If option is:
+   *   - STREAM_META_TOUCH: Array consisting of two arguments of the touch()
+   *     function.
+   *   - STREAM_META_OWNER_NAME or STREAM_META_GROUP_NAME: The name of the owner
+   *     user/group as string.
+   *   - STREAM_META_OWNER or STREAM_META_GROUP: The value of the owner
+   *     user/group as integer.
+   *   - STREAM_META_ACCESS: The argument of the chmod() as integer.
+   *
+   * @return bool
+   *   Returns TRUE on success or FALSE on failure. If $option is not
+   *   implemented, FALSE should be returned.
+   *
+   * @see http://www.php.net/manual/streamwrapper.stream-metadata.php
+   */
+  public function stream_metadata($path, $option, $value) {
+    return FALSE;
+  }
+  
+ 
+   /**
+   * Change stream options.
+   *
+   * This method is called to set options on the stream.
+   *
+   * @param int $option
+   *   One of:
+   *   - STREAM_OPTION_BLOCKING: The method was called in response to
+   *     stream_set_blocking().
+   *   - STREAM_OPTION_READ_TIMEOUT: The method was called in response to
+   *     stream_set_timeout().
+   *   - STREAM_OPTION_WRITE_BUFFER: The method was called in response to
+   *     stream_set_write_buffer().
+   * @param int $arg1
+   *   If option is:
+   *   - STREAM_OPTION_BLOCKING: The requested blocking mode:
+   *     - 1 means blocking.
+   *     - 0 means not blocking.
+   *   - STREAM_OPTION_READ_TIMEOUT: The timeout in seconds.
+   *   - STREAM_OPTION_WRITE_BUFFER: The buffer mode, STREAM_BUFFER_NONE or
+   *     STREAM_BUFFER_FULL.
+   * @param int $arg2
+   *   If option is:
+   *   - STREAM_OPTION_BLOCKING: This option is not set.
+   *   - STREAM_OPTION_READ_TIMEOUT: The timeout in microseconds.
+   *   - STREAM_OPTION_WRITE_BUFFER: The requested buffer size.
+   *
+   * @return bool
+   *   TRUE on success, FALSE otherwise. If $option is not implemented, FALSE
+   *   should be returned.
+   */
+  public function stream_set_option($option, $arg1, $arg2) {
+    return FALSE;
+  }
+ 
+  /**
+   * Truncate stream.
+   *
+   * Will respond to truncation; e.g., through ftruncate().
+   *
+   * @param int $new_size
+   *   The new size.
+   *
+   * @return bool
+   *   TRUE on success, FALSE otherwise.
+   *
+   * @todo
+   *   This one actually makes sense for the example.
+   */
+  public function stream_truncate($new_size) {
+    return FALSE; 
+  }    
 
   /**
    * Support for flock().
@@ -422,7 +526,7 @@ class FileExampleSessionStreamWrapper implements StreamWrapperInterface {
    *
    * @see http://php.net/manual/en/streamwrapper.stream-seek.php
    */
-  public function stream_seek($offset, $whence) {
+  public function stream_seek($offset, $whence = SEEK_SET) {
     if (drupal_strlen($this->sessionContent) >= $offset) {
       $this->streamPointer = $offset;
       return TRUE;
